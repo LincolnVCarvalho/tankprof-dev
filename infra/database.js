0294030b2a198d520fb16a459c91b86251e4a248
Lincoln Carvalho <liincollnv@hotmail.com>
2025-12-17 00:45:31 -0300

    Added `database` to the project

---
+++ infra/database.js
@@ -0,0 +1,28 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+    });
+
+    await client.connect();
+
+    try {
+        const response = await client.query(queryObject);
+        await client.end();
+        return response;
+    } catch (error) {
+        console.error("Database query error:", error);
+        throw error;
+    } finally {
+        await client.end();
+    }
+}
+
+export default {
+    query: query,
+};
